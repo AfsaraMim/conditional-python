fec29392b023bf83e52e136d23beb75309a3fc7a
Afsara Anjum Benthy Amin Mim <afsaraanjummim11@gmail.com>
2024-08-30 14:23:15 +0600

    Create else.py

---
+++ else.py
@@ -0,0 +1,9 @@
+classwork_solve=int(input("Enter the solution num of your class work(1-3):"))
+if classwork_solve==3:
+    print("You have gotten a Triangle") 
+elif classwork_solve==2:
+    print("You have gotten a Circle")
+elif classwork_solve==1:
+    print("You have gotten a Rectangular")
+else:
+    print("You could not gain any performance indicator")
