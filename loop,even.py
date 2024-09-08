f3c234548d6eeec272bd9ed9d0f98eb86edbfee9
Afsara Anjum Benthy Amin Mim <afsaraanjummim11@gmail.com>
2024-09-08 18:39:32 +0600

    Create loop,even.py

---
+++ loop,even.py
@@ -0,0 +1,9 @@
+# Initialize the starting number
+ = 1
+
+# Start the while loop
+while number <= 100:
+    # Check if the number is even
+    if number % 2 == 0:number
+        print(number)  # Print the current even number
+    number += 1      # Move to the next number
