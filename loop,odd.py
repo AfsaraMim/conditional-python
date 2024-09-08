f93e656b770f06d0684166c67789a6aec8463acc
Afsara Anjum Benthy Amin Mim <afsaraanjummim11@gmail.com>
2024-09-08 18:45:53 +0600

    Create loop,odd.py

---
+++ loop,odd.py
@@ -0,0 +1,9 @@
+# Initialize the starting number
+number = 1
+
+# Start the while loop
+while number <= 100:
+    # Check if the number is odd
+    if number % 2 != 0:
+        print(number)  # Print the current odd number
+    number += 1      # Move to the next number
